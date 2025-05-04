7b34bf250667c65e91ce9a142f3adafdc74211fc
Anshusharma42019 <anshusharma42019@gmail.com>
2025-05-05 00:13:47 +0530

    complete backend

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,20 @@
 from flask import Flask, jsonify
 from .content_api import content_bp
+from flask_cors import CORS  # <-- import CORS
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env file
+# Load environment variables
 load_dotenv()
 
-# Accessing environment variables
 SECRET_KEY = os.getenv('SECRET_KEY')
-API_URL = os.getenv('API_URL')  # This will be used in your Flask routes
-
+API_URL = os.getenv('API_URL')
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, origins=["http://localhost:5173"], supports_credentials=True)  # <-- Add this line
+
     app.register_blueprint(content_bp)
 
     @app.route('/')
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from content_api import content_bp # correct if content_api.py is in the same folder
 
+
 app = Flask(__name__)
 
 # Register the blueprint
